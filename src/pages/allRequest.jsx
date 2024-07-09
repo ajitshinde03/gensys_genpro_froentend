c3fe313b6f25944040783e6e81247d2087bbbadd
gunapatirao <gunapati_rao@persistent.com>
2024-07-09 20:08:23 +0530

    added json data for table data,button action and css changes in all requests page

--- src/pages/allRequest.jsx
+++ src/pages/allRequest.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch, BsInfoCircle } from "react-icons/bs";
-import { IoIosMenu,IoMdTime } from "react-icons/io";
-import { RxCross2  } from "react-icons/rx";
+import { IoIosMenu, IoMdTime } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 import { GoCheck } from "react-icons/go";
+import { TbClock } from "react-icons/tb";
 import "./allRequest.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import { TableData } from "./tabledata";
 
-
+// const handleShow = () => setShow(true);
 const request_counts = {
-  "allReuests": "100",
-  "pending": "30",
-  "approved": "30",
-  "rejected": "40"
-
-}
+  allReuests: "100",
+  pending: "30",
+  approved: "30",
+  rejected: "40",
+};
 // const request_Data = [{
 //   {"target_view_name": "Finance",
 //     "target_schema_name": "b_ecc",
 //     "catlog": "adh_hh_d_dev",
 //     "schema": "b-ecc",
 //     "table": "",
-//     // "environment":   
+//     // "environment":
 // }]
-const approve =() =>{
-  alert("Approved button clicked")
-}
-const rejected =() =>{
-  alert("Rejected button clicked!!")
-}
+
+const approve = () => {
+  alert("Approved button clicked");
+};
+const rejected = () => {
+  alert("Rejected button clicked!!");
+};
 const MyView = () => {
+  const [modalShow, setModalShow] = useState(false);
+
+  const handleShow = () => setModalShow(true);
+  const handleClose = () => setModalShow(false);
+
+  // Approved Buttons
+  // const status = "approved";
+  // let successButtonVariant;
+
   return (
-    <div className="container m-4">
-      
+    <div className="main-container m-4">
       <h6 className="mt-2 last_update">Last Updated : 10:00:00 AM</h6>
       <h2>All Request</h2>
       <div className="d-flex my-4" style={{ width: "72rem" }}>
@@ -45,13 +59,15 @@ const MyView = () => {
         </button>
       </div>
 
-      <div className="ml-1 row">
-        <div class="col-md-8 col-md-8 card">
+      <div className="ml-1 row all-request-report">
+        <div class="col-md-9 col-md-9 card">
           <div class="card-body d-flex row">
             <div className=" col-md-3">
               <span className="d-flex">
                 <IoIosMenu className="icon" />
-                <h5 className="request_count ml-2">{request_counts.allReuests}</h5>
+                <h5 className="request_count ml-2">
+                  {request_counts.allReuests}
+                </h5>
               </span>
               <h6>All Request</h6>
             </div>
@@ -67,22 +83,26 @@ const MyView = () => {
             <div className=" col-md-3">
               <span className="d-flex">
                 <GoCheck className="icon approved_color" />
-                <h5 className="request_count ml-2">{request_counts.approved}</h5>
+                <h5 className="request_count ml-2">
+                  {request_counts.approved}
+                </h5>
               </span>
               <h6 className="approved_color">Approved</h6>
             </div>
 
             <div className=" col-md-3">
               <span className="d-flex">
-                <RxCross2  className="icon rejected_color" />
-                <h5 className="request_count ml-2">{request_counts.rejected }</h5>
+                <RxCross2 className="icon rejected_color" />
+                <h5 className="request_count ml-2">
+                  {request_counts.rejected}
+                </h5>
               </span>
               <h6 className="rejected_color">Rejected</h6>
             </div>
           </div>
         </div>
 
-        <div className="col-md-4 ">
+        <div className="col-md-3">
           <nav aria-label="Page navigation example float-right">
             <ul class="pagination">
               <li class="page-item">
@@ -117,7 +137,7 @@ const MyView = () => {
         </div>
       </div>
 
-      <table class="table table-bordered my-4">
+      <table class="table table-responsive table-bordered my-4">
         <thead class="thead-light thead-light_blue">
           <tr>
             <th scope="col">Target view name</th>
@@ -129,75 +149,94 @@ const MyView = () => {
             <th scope="col">Type</th>
             <th scope="col">Region</th>
             <th scope="col">Created Date</th>
-            <th scope="col">Created Date</th>
             <th scope="col">Last Updated</th>
             <th scope="col">Approved By</th>
             <th scope="col">Status</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td className="d-flex">
-              <button onClick={approve} type="button" class="btn btn-success m-2">
-                <GoCheck className="icon" />
-              </button>
-              <button onClick={rejected} type="button" class="btn btn-danger m-2">
-                <RxCross2  className="icon" />
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>
-              <button type="button" class="btn btn-outline-success">
-                Approved
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>
-              <button type="button" class="btn btn-outline-danger">
-                Rejected <BsInfoCircle className="icon" />
-              </button>
-            </td>
-          </tr>
+          {TableData.data.map((data) => {
+            return (
+              <tr>
+                <th scope="row">{data.view_name}</th>
+                <td>{data.target_schema_name}</td>
+                <td>{data.catelog_name}</td>
+                <td>{data.schema_name}</td>
+                <td>{data.table_name}</td>
+                <td>{data.env_name}</td>
+                <td>{data.type_name}</td>
+                <td>{data.region_name}</td>
+                <td>{data.created_date}</td>
+                <td>{data.updated_date}</td>
+                <td>{data.Approved_by}</td>
+                <td>
+                  {data.status === "Approved" && (
+                    <Button
+                      variant="outline-success"
+                      disabled
+                      className="custom-button-success"
+                    >
+                      Approved
+                    </Button>
+                  )}
+                  {data.status === "Rejected" && (
+                    <Button
+                      variant="outline-danger"
+                      disabled
+                      className="custom-button-danger"
+                    >
+                      Rejected
+                      <TbClock className="clock-icon" />
+                    </Button>
+                  )}
+                  {data.status === "Pending" && (
+                    <div className="d-flex action-buttons">
+                      <Button
+                        variant="success"
+                        // onClick={""}
+                        class="btn btn-success m-2"
+                      >
+                        <GoCheck className="icon" />
+                      </Button>{" "}
+                      <Button
+                        variant="danger"
+                        onClick={handleShow}
+                        class="btn btn-danger m-2"
+                      >
+                        <RxCross2 className="icon" />
+                      </Button>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      <Modal
+        show={modalShow}
+        onHide={handleClose}
+        backdrop="sm"
+        arial-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body>
+          <h4 className="rej-content">
+            Are you sure you want to Reject the <br />
+            View?
+          </h4>
+
+          <h4>Rejection Reason</h4>
+          <Form>
+            <Form.Group controlId="fromBasicText">
+              <Form.Control type="text" className="custom-input" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
---
+++ src/pages/tabledata.jsx
@@ -0,0 +1,96 @@
+export const TableData = {
+  status: "success",
+
+  message: "All Request details fetched successfully",
+
+  data: [
+    {
+      id: 1,
+
+      type_name: "gensys",
+
+      env_name: "dev",
+
+      region_name: "NORTHERN REGION",
+
+      catelog_name: "system",
+
+      schema_name: "information_schema",
+
+      table_name: "catalog_privileges",
+
+      target_schema_name: "invoice",
+
+      view_name: "testing first entry",
+
+      status: "Approved",
+
+      Approved_by: "Admin",
+
+      reject_reason: "nothing",
+
+      created_date: "2024-07-06T22:05:45",
+
+      updated_date: "2024-07-08T16:11:57",
+    },
+
+    {
+      id: 2,
+
+      type_name: "gensys",
+
+      env_name: "dev",
+
+      region_name: "NORTHERN REGION",
+
+      catelog_name: "system",
+
+      schema_name: "information_schema",
+
+      table_name: "catalog_privileges",
+
+      target_schema_name: "Information_schema",
+
+      view_name: "2nd entry",
+
+      status: "Rejected",
+
+      Approved_by: "Admin",
+
+      reject_reason: "nothing Rohit",
+
+      created_date: "2024-07-08T16:02:06",
+
+      updated_date: "2024-07-08T16:21:51",
+    },
+    {
+      id: 3,
+
+      type_name: "gensys",
+
+      env_name: "dev",
+
+      region_name: "NORTHERN REGION",
+
+      catelog_name: "system",
+
+      schema_name: "information_schema",
+
+      table_name: "catalog_privileges",
+
+      target_schema_name: "Information_schema",
+
+      view_name: "3rd entry",
+
+      status: "Pending",
+
+      Approved_by: "Admin",
+
+      reject_reason: "nothing Rohit",
+
+      created_date: "2024-07-08T16:02:06",
+
+      updated_date: "2024-07-08T16:21:51",
+    },
+  ],
+};
